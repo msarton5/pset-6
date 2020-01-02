1f69dff58b71eb4ae8995561b4777ea018e6adfe
Molly Sarton <MSarton@ucvts.tec.nj.us>
2020-01-02 13:23:08 -0500

    commit 2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
 window.onload = function() {
     document.getElementById("").onclick = ;
 }
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Problem Set 5</title>
+
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
+
+    <script type="text/javascript" src="js/app.js"></script>
+
+</head>
+<body>
+    <div class="header">
+        <h1>#TODO</h1>
+        <hr />
+    </div>
+    <div class="exercise" id="exercise-1">
+        <div>
+            <button id=""></button>
+        </div>
+
+        <h3>STUDENT OUTPUT</h3>
+        <p class="output" id="output-1">
+            <canvas width="1024px" height="128px" id="student-canvas-1">
+
+            </canvas>
+        </p>
+    </div>
+  </body>
+</html>
