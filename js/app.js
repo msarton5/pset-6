4a1de4e0f19328cb5b14ad9ca85ae661dbefc0b4
Lalena Sarton <lalena@lalenas-mbp-2.myfiosgateway.com>
2020-01-05 15:47:11 -0500

    another another commit

--- js/app.js
+++ js/app.js
@@ -3,7 +3,12 @@ window.onload = function() {
 }
 
 const addItemToList = function() {
-  var input = document.getElementById("addButton");
-  alert(input);
+  // var input = document.getElementById("addText");
+  let canvas = document.getElementById("toDoCanvas");
+  let ctx = canvas.getContext("2d");
 
+let text = prompt("Message:");
+
+  ctx.font = "48px Sans-serif";
+  ctx.strokeText(text, 30 , 70, canvas.width - 30);
 }
