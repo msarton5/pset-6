13641deeb7e64c88e339ea07e28c7a8be77d10ff
Lalena Sarton <lalena@Lalenas-MacBook-Pro-2.local>
2020-01-06 22:34:55 -0500

    fixing buttons

--- js/app.js
+++ js/app.js
@@ -6,10 +6,6 @@ const chores = [];
 
 const addChoreToList = function() {
   const text = document.getElementById("addText").value;
-  // let canvas = document.getElementById("toDoCanvas");
-
-  // let text = prompt("Message:");
-
   const chore = {
     id: chores.length,
     priority: "low",
@@ -34,14 +30,35 @@ const renderChores = function() {
     const span3 = document.createElement("span");
     const span4 = document.createElement("span");
 
-    span1.innerHTML = "!";
+    span1.innerHTML = "!"; // exclamation point
+      span1.onclick = prioritizeListItem();
+      span1.setAttribute("id", "spanOne");
     span2.innerHTML = chores[i].content;
+      span2.setAttribute("id", "spanTwo");
     span3.innerHTML = "&#10004;"; // check mark
-    // span3.onclick =
-    span3.className = chores[i].complete === true ? "done" : "not-done";
+      span3.className = chores[i].complete === true ? "done" : "not-done";
+      span3.onclick = markAsDone();
+      span3.setAttribute("id" , "spanThree");
     span4.innerHTML = "&#10006;"; // x
-    // span4.onclick =
-    span4.id = "delete-" + chores[i].id;
+      span4.onclick = makeTheThingDisappear();
+      span4.setAttribute("id" , "spanFour");
+      span4.id = "delete-" + chores[i].id;
+
+const prioritizeListItem = function() {
+  span1.setAttribute("id" , "spanOne");
+
+}
+
+const markAsDone = function() {
+  span3.setAttribute("id" , "spanThree");
+}
+
+const makeTheThingDisappear = function() {
+  span4.setAttribute("id" , "spanFour");
+  var element = document.getElementById(spanThree);
+    element.parentNode.removeChild(spanOne);
+    element.parentNode.removeChild(spanFour);
+}
 
     li.append(span1);
     li.append(span2);
