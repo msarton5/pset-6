8c44f5698da9c65e736c6ba96cd04569a4ee319e
Lalena Sarton <lalena@Lalenas-MacBook-Pro-2.local>
2020-01-07 22:44:27 -0500

    uygdvbjd

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ function renderChores2() {
     const xMark = document.createElement("BUTTON");
 
     exclamation.innerHTML = "!"; // exclamation point
-      exclamation.onclick = prioritizeListItem();
+    //  exclamation.onclick = prioritizeListItem();
       exclamation.style.fontWeight = "bold";
     userInput.innerHTML = chores[i].content;
       userInput.style.border = "2px solid black";
@@ -64,25 +64,127 @@ function renderChores2() {
 }
 
 const prioritizeListItem = function() {
-  chores.priority = "high";
-  renderChores2();
+  chore.priority = "high";
+  const list = document.getElementById("list");
+  list.innerHTML = "";
 
+  for (let i = 0; i < chores.length; i++) {
+    const li = document.createElement("li");
+    const exclamation = document.createElement("BUTTON");
+    const userInput = document.createElement("span");
+    const checkMark = document.createElement("BUTTON");
+    const xMark = document.createElement("BUTTON");
+
+    exclamation.innerHTML = "!"; // exclamation point
+      // exclamation.onclick = prioritizeListItem();
+      exclamation.style.fontWeight = "bold";
+    userInput.innerHTML = chores[i].content;
+      userInput.style.border = "2px solid black";
+      userInput.style.background = "#ffc0cb";
+    checkMark.innerHTML = "&#10004;"; // check mark
+      checkMark.className = chores[i].complete === true ? "done" : "not-done";
+      checkMark.style.color = "green";
+      //checkMark.onclick = markAsDone();
+    xMark.innerHTML = "&#10006;"; // x
+      xMark.style.color = "red";
+      //xMark.onclick = makeTheThingDisappear();
+    exclamation.setAttribute("id" , "prioritize");
+    userInput.setAttribute("id" , "list");
+    checkMark.setAttribute("id" , "check");
+    xMark.setAttribute("id" , "del");
+
+    li.append(exclamation);
+    li.append(userInput);
+    li.append(checkMark);
+    li.append(xMark);
+    list.append(li);
+    console.log(li);
+  }
 }
 
 const markAsDone = function() {
   document.getElementById("userInput").style.background = "green";
-  renderChores2();
+  const list = document.getElementById("list");
+  list.innerHTML = "";
+
+  for (let i = 0; i < chores.length; i++) {
+    const li = document.createElement("li");
+    const exclamation = document.createElement("BUTTON");
+    const userInput = document.createElement("span");
+    const checkMark = document.createElement("BUTTON");
+    const xMark = document.createElement("BUTTON");
+
+    exclamation.innerHTML = "!"; // exclamation point
+      // exclamation.onclick = prioritizeListItem();
+      exclamation.style.fontWeight = "bold";
+    userInput.innerHTML = chores[i].content;
+      userInput.style.border = "2px solid black";
+      userInput.style.background = "#ffc0cb";
+    checkMark.innerHTML = "&#10004;"; // check mark
+      checkMark.className = chores[i].complete === true ? "done" : "not-done";
+      checkMark.style.color = "green";
+      //checkMark.onclick = markAsDone();
+    xMark.innerHTML = "&#10006;"; // x
+      xMark.style.color = "red";
+      //xMark.onclick = makeTheThingDisappear();
+    exclamation.setAttribute("id" , "prioritize");
+    userInput.setAttribute("id" , "list");
+    checkMark.setAttribute("id" , "check");
+    xMark.setAttribute("id" , "del");
+
+    li.append(exclamation);
+    li.append(userInput);
+    li.append(checkMark);
+    li.append(xMark);
+    list.append(li);
+    console.log(li);
+  }
 }
 
 const makeTheThingDisappear = function() {
-  var remove = document.getElementById(exclamation);
-    exclamation.parentNode.removeChild(exclamation);
-  var remove2 = document.getElementById(list);
-    remove2.parentNode.removeChild(remove2);
-  var remove3 = document.getElementById(check);
-    check.parentNode.removeChild(check);
-  var remove4 = document.getElementById(del);
-    del.parentNode.removeChild(del);
+  var element = document.getElementById(prioritize);
+      element.remove();
+  var elementTwo = document.getElementById(list);
+      element.remove();
+  var elementThree = document.getElementById(check);
+      element.remove();
+  var element = document.getElementById(del);
+      element.remove();
 
-    renderChores2();
+
+    const list = document.getElementById("list");
+    list.innerHTML = "";
+
+    for (let i = 0; i < chores.length; i++) {
+      const li = document.createElement("li");
+      const exclamation = document.createElement("BUTTON");
+      const userInput = document.createElement("span");
+      const checkMark = document.createElement("BUTTON");
+      const xMark = document.createElement("BUTTON");
+
+      exclamation.innerHTML = "!"; // exclamation point
+      //  exclamation.onclick = prioritizeListItem();
+        exclamation.style.fontWeight = "bold";
+      userInput.innerHTML = chores[i].content;
+        userInput.style.border = "2px solid black";
+        userInput.style.background = "#ffc0cb";
+      checkMark.innerHTML = "&#10004;"; // check mark
+        checkMark.className = chores[i].complete === true ? "done" : "not-done";
+        checkMark.style.color = "green";
+        //checkMark.onclick = markAsDone();
+      xMark.innerHTML = "&#10006;"; // x
+        xMark.style.color = "red";
+        //xMark.onclick = makeTheThingDisappear();
+      exclamation.setAttribute("id" , "prioritize");
+      userInput.setAttribute("id" , "list");
+      checkMark.setAttribute("id" , "check");
+      xMark.setAttribute("id" , "del");
+
+      li.append(exclamation);
+      li.append(userInput);
+      li.append(checkMark);
+      li.append(xMark);
+      list.append(li);
+      console.log(li);
+    }
 }
